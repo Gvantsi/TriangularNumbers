488b929565b1271e93d8f7c9789f595a7ffac11f
GVANT$A <gvantsa.beridze.2@btu.edu.ge>
2021-03-16 00:22:57 +0400

    Add files via upload

---
+++ Hellojava/src/com/hello/triangularNumber.java
@@ -0,0 +1,26 @@
+package com.hello;
+
+public class triangularNumber{
+
+	public static void main(String[] args) {
+		
+		
+	
+int x =1;
+while (x <= 10) {
+	int triangularNumber = 1;
+	
+	while (x <= 10 ) {
+		
+		System.out.print(triangularNumber);
+		
+		x++;
+		
+		triangularNumber = triangularNumber +x;
+	}
+}
+		
+		
+	}
+
+}
